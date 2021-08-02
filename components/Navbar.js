abcb58202033e17423dc3333a4969b9ee4382bac
hungUET231 <hungngv231@gmail.com>
2021-08-03 04:27:17 +0700

    fix mobile menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -101,12 +101,16 @@ const Navbar = () => {
             <div style={{ textAlign: "center", marginTop: "10px" }}>
               {Object.keys(auth).length === 0 ? (
                 <div style={{ padding: "0 16px" }}>
-                  <div className={styles.signUpBtn} onClick={onClose}>
-                    <Link href="/register">Đăng ký</Link>
-                  </div>
-                  <div className={styles.loginBtn} onClick={onClose}>
-                    <Link href="/login">Đăng nhập</Link>
-                  </div>
+                  <Link href="/register" passHref>
+                    <div className={styles.signUpBtn} onClick={onClose}>
+                      Đăng ký
+                    </div>
+                  </Link>
+                  <Link href="/login" passHref>
+                    <div className={styles.loginBtn} onClick={onClose}>
+                      Đăng nhập
+                    </div>
+                  </Link>
                 </div>
               ) : (
                 <>
