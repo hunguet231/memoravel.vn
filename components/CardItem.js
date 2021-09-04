0dbee9b4049d1a0477388064f76467d722b894da
nghialm <lmn2461999@gmail.com>
2021-09-05 02:15:25 +0700

    fix bug ui

--- components/CardItem.js
+++ components/CardItem.js
@@ -6,39 +6,41 @@ import Link from "next/link";
 
 export default function CardItem({ product }) {
   return (
-    <div>
+    <div className={styles.borderCard}>
       <Link href={`/products/${product && product._id}`} passHref>
         <Card
           className={styles.card}
           hoverable
           bordered={false}
+          bodyStyle={{ height: "calc(100% - 300px)", padding: "4px 24px 24px" }}
           cover={
-            <>
-              <div className={styles.cardImg}>
-                <Image
-                  preview={false}
-                  alt="items"
-                  src={product && product.images[0]}
-                />
-                <div className={styles.ar}>AR</div>
-              </div>
-            </>
+            <div className={styles.cardImg}>
+              <Image
+                width="100%"
+                preview={false}
+                alt="items"
+                src={product && product.images[0]}
+              />
+              <div className={styles.ar}>AR</div>
+            </div>
           }
         >
           <div className={styles.cardBody}>
             <p className={styles.cardTitle}>{product && product.title}</p>
-            <Row justify="space-between">
-              <Col>
-                <Rate disabled defaultValue={5} style={{ fontSize: "95%" }} />
-              </Col>
-              <Col>
-                <small className={styles.sold}>
-                  Đã bán: {product && product.sold}
-                </small>
-              </Col>
-            </Row>
-            <div className={styles.summary}>
-              <small>{ReactHtmlParser(product && product.summary)}</small>
+            <div>
+              <Row justify="space-between">
+                <Col>
+                  <Rate disabled defaultValue={5} style={{ fontSize: "95%" }} />
+                </Col>
+                <Col>
+                  <small className={styles.sold}>
+                    Đã bán: {product && product.sold}
+                  </small>
+                </Col>
+              </Row>
+              <div className={styles.summary}>
+                <small>{ReactHtmlParser(product && product.summary)}</small>
+              </div>
             </div>
             <Row justify="end">
               <Col className={styles.price}>
