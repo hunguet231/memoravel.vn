dc2fa58d507b273db6f3d467c036a313cd086bca
cnhung2001 <63276586+cnhung2001@users.noreply.github.com>
2021-09-29 22:38:32 +0700

    update models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,36 @@
-import UserModel from "./user.model";
+const dotenv = require("dotenv");
+dotenv.config();
+const Sequelize = require("sequelize");
 
-export { UserModel };
+const env = process.env.NODE_ENV || "development";
+const config = require("../configs/config.json")[env];
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+const db = {};
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.user = require("./user.model")(sequelize, Sequelize);
+db.post = require("./post.model")(sequelize, Sequelize);
+db.topic = require("./topic.model")(sequelize, Sequelize);
+
+// Join user with post
+db.user.hasOne(db.post);
+db.post.belongsTo(db.user);
+//Join post with topic
+db.post.hasMany(db.topic);
+db.topic.belongsTo(db.post);
+
+module.exports = db;
