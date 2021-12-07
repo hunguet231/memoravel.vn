d81c00a20d2f905363fb4f1c5ed416d49d894e25
hungUET231 <hungngv231@gmail.com>
2021-12-08 05:14:39 +0700

    update admin, post

--- client/src/components/admin/post/DialogPost.js
+++ client/src/components/admin/post/DialogPost.js
@@ -1,19 +1,20 @@
 /* eslint-disable react/prop-types */
 import {
+  CircularProgress,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
+  FormControl,
+  FormControlLabel,
   IconButton,
   makeStyles,
   OutlinedInput,
-  Typography,
-  CircularProgress,
-  RadioGroup,
-  FormControlLabel,
-  FormControl,
   Radio,
+  RadioGroup,
+  Typography,
 } from "@material-ui/core";
+import { Alert, AlertTitle } from "@material-ui/lab";
 import { Close, CloudUpload as CloudUploadIcon } from "@material-ui/icons";
 import { CKEditorComponent } from "components";
 import Button from "components/common/Button";
@@ -79,6 +80,13 @@ const DialogPost = ({ isShow, onClose, onSubmit, data, topics, loading }) => {
     });
   };
 
+  const onDescChange = (data) => {
+    setDataInput({
+      ...dataInput,
+      description: data,
+    });
+  };
+
   useEffect(() => {
     if (!!data) {
       setDataInput(data);
@@ -136,12 +144,16 @@ const DialogPost = ({ isShow, onClose, onSubmit, data, topics, loading }) => {
           onChange={onTypingData}
         />
         <Typography className={classes.typographyContent}>Mô tả</Typography>
-        <OutlinedInput
+        <CKEditorComponent
+          name="content"
+          onChange={onDescChange}
+          data={dataInput?.description || ""}
+        />
+        {/* <OutlinedInput
           placeholder="Nhập mô tả"
           multiline
           classes={{
-            root: classes.contentLineEdit,
-            input: classes.inputEdit,
+            root: classes.textAreaInput,
             disabled: classes.disabled,
           }}
           required
@@ -150,7 +162,7 @@ const DialogPost = ({ isShow, onClose, onSubmit, data, topics, loading }) => {
           }}
           value={dataInput?.description || ""}
           onChange={onTypingData}
-        />
+        /> */}
         <Typography className={classes.typographyContent}>Ảnh cover</Typography>
 
         <Button
@@ -172,6 +184,18 @@ const DialogPost = ({ isShow, onClose, onSubmit, data, topics, loading }) => {
         )}
 
         <Typography className={classes.typographyContent}>Nội dung</Typography>
+        <Alert severity="warning">
+          <AlertTitle>Chú ý</AlertTitle>
+          Chèn link ảnh từ <mark>Google Drive:</mark>
+          <br /> Với link ảnh &quot;https://drive.google.com/file/d/
+          <mark>1DYHYl8mPcA_C_EONLG3sAXBOUJhM-xyg</mark>/...&quot; thì FILE_ID
+          sẽ là <mark>1DYHYl8mPcA_C_EONLG3sAXBOUJhM-xyg</mark>
+          <br /> Thay FILE_ID trên vào
+          &quot;https://drive.google.com/uc?id=FILE_ID&quot; sẽ được link ảnh
+          đúng
+          <br /> &quot;https://drive.google.com/uc?id=
+          <mark>1DYHYl8mPcA_C_EONLG3sAXBOUJhM-xyg</mark>&quot;
+        </Alert>
         <CKEditorComponent
           name="content"
           onChange={onCKChange}
@@ -361,4 +385,16 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "right",
     padding: "16px 24px",
   },
+  // textAreaInput: {
+  //   fontSize: 14,
+  //   padding: "9px 16px",
+  //   marginBottom: 16,
+  //   "&$disabled": {
+  //     backgroundColor: "#d4d5d8",
+  //     opacity: 0.3,
+  //     color: "#565c6a",
+  //     border: "none",
+  //     cursor: "no-drop",
+  //   },
+  // },
 }));
