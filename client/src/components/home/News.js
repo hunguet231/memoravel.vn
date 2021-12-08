0befb073d83f661543b4b4dda6447fafe7ece538
hungUET231 <hungngv231@gmail.com>
2021-12-08 10:46:11 +0700

    update home news

--- client/src/components/home/News.js
+++ client/src/components/home/News.js
@@ -1,68 +1,50 @@
+/* eslint-disable react/prop-types */
 import BoxImage from "components/common/BoxImage";
 import Button from "components/common/Button";
 import Tag from "components/common/Tage";
 import React from "react";
+import Link from "next/link";
+import useWindowSize from "utils/useWindowSize";
 import styles from "../../styles/NewsHome.module.scss";
 
-const News = () => {
+const News = ({ news }) => {
+  const size = useWindowSize();
   return (
     <div className="wrapper">
       <div className="container">
         <div className={styles.news}>
           <div className="flex items-center justify-between">
             <h1 className="heading heading-section">Tin tức mới nhất</h1>
-            <p className={styles.more}>Xem thêm</p>
+            <Link href={`/blogs`}>
+              <p className={styles.more}>Xem thêm</p>
+            </Link>
           </div>
           <div className={styles.boxes}>
-            <BoxImage
-              className={styles.box1}
-              imgUrl={"/images/new1.png"}
-              subHeading={
-                <>
-                  <Tag text="TIPS" /> <span>15/10/2021</span>{" "}
-                </>
-              }
-              heading={<>5 bước trang trí nhà hợp phong thủy</>}
-              textMore={"Đọc thêm"}
-            />
-            <BoxImage
-              className={styles.box2}
-              imgUrl={"/images/new2.png"}
-              subHeading={
-                <>
-                  <Tag text="TIPS" /> <span>14/10/2021</span>{" "}
-                </>
-              }
-              heading={<>Lối sống tối giản của người Nhật</>}
-              textMore={"Đọc thêm"}
-            />
-            <BoxImage
-              className={styles.box3}
-              imgUrl={"/images/new3.png"}
-              subHeading={
-                <>
-                  <Tag text="HOT" /> <span>12/10/2021</span>{" "}
-                </>
-              }
-              heading={
-                <>Bình gốm Việt Nam đang được đấu giá lên đến 20000 đô</>
-              }
-              textMore={"Đọc thêm"}
-            />
-            <BoxImage
-              className={styles.box4}
-              imgUrl={"/images/new4.png"}
-              subHeading={
-                <>
-                  <Tag text="TIPS" /> <span>8/10/2021</span>{" "}
-                </>
-              }
-              heading={<>Những thú vui khi về hưu (phần 1)</>}
-              textMore={"Đọc thêm"}
-            />
+            {news &&
+              news.length > 0 &&
+              news.map((post) => (
+                <BoxImage
+                  key={post.id}
+                  className={styles.box}
+                  imgUrl={post.background}
+                  subHeading={
+                    <>
+                      {size.width > 600 && <Tag text={post.topics[0].title} />}
+                      <span>
+                        {new Date(post.created).toLocaleDateString()}
+                      </span>{" "}
+                    </>
+                  }
+                  heading={post.title}
+                  textMore={"Đọc thêm"}
+                  link={post.alias}
+                />
+              ))}
           </div>
           <div className={styles.button}>
-            <Button type="secondary">Xem tất cả</Button>
+            <Link href={`/blogs`}>
+              <Button type="secondary">Xem tất cả</Button>
+            </Link>
           </div>
         </div>
       </div>
