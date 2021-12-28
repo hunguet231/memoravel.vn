e49123d6fc0222506fe2d84aad92af70b820cb4f
manhhungpc <phamhung2002@gmail.com>
2021-12-28 23:13:18 +0700

    add products api

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -42,7 +42,83 @@ export const createProduct = async (req, res) => {
     res
       .status(AppConst.STATUS_CREATED)
       .json(responseFormat({ data: formatProductData(getProduct) }));
-    //continue here
+  } catch (error) {
+    res
+      .status(AppConst.STATUS_SERVER_ERROR)
+      .json(responseFormat({ error: error, message: 'error' }));
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    await Product.update(req.body, {
+      where: { id: req.params.product_id },
+    });
+    const getProduct = await findProductById(req.params.product_id);
+    res
+      .status(AppConst.STATUS_CREATED)
+      .json(responseFormat({ data: formatProductData(getProduct) }));
+  } catch (error) {
+    res
+      .status(AppConst.STATUS_SERVER_ERROR)
+      .json(responseFormat({ error: error, message: 'error' }));
+  }
+};
+
+export const getAllProduct = async (req, res) => {
+  try {
+    const dataPage = convertPaging(req);
+    const pagination =
+      dataPage.paging === 0
+        ? {}
+        : {
+            limit: dataPage.size,
+            offset: (dataPage.page - 1) * dataPage.size,
+          };
+
+    const queryDataProduct = {};
+    if (dataPage.search) {
+      queryDataProduct.name = {
+        [Op.like]: `%${dataPage.search}%`,
+      };
+    }
+
+    const { count, rows: data } = await Product.findAndCountAll({
+      ...pagination,
+      where: {
+        ...queryDataProduct,
+      },
+      include: [{ model: Shop, attributes: ['id', 'name'] }],
+      distinct: true,
+    });
+
+    const formatData = data.map((item) => formatProductData(item));
+
+    const response =
+      dataPage.paging === 0
+        ? {
+            data: formatData,
+            total: count,
+          }
+        : {
+            data: formatData,
+            total: count,
+            page: dataPage.page,
+          };
+    res.status(AppConst.STATUS_OK).json(responseFormat(response));
+  } catch (error) {
+    res
+      .status(AppConst.STATUS_SERVER_ERROR)
+      .json(responseFormat({ error: error, message: 'error' }));
+  }
+};
+
+export const getProductById = async (req, res) => {
+  try {
+    const data = await findProductById(req.params.product_id);
+
+    const formatData = formatProductData(data);
+    res.status(AppConst.STATUS_OK).json(responseFormat({ data: formatData }));
   } catch (error) {
     res
       .status(AppConst.STATUS_SERVER_ERROR)
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -12,4 +12,28 @@ productRoute.post(
   ProductMiddleware.checkCreateProduct,
   ProductController.createProduct
 );
+
+productRoute.put(
+  ApiConst.MANAGE_PRODUCT_ID,
+  CommonMiddleware.verifyToken,
+  CommonMiddleware.verifyRolesManage,
+  ProductMiddleware.checkUpdateProduct,
+  ProductController.updateProduct
+);
+
+productRoute.get(
+  ApiConst.MANAGE_PRODUCT,
+  CommonMiddleware.verifyToken,
+  CommonMiddleware.verifyRolesManage,
+  ProductController.getAllProduct
+);
+
+productRoute.get(
+  ApiConst.MANAGE_PRODUCT_ID,
+  CommonMiddleware.verifyToken,
+  CommonMiddleware.verifyRolesManage,
+  ProductMiddleware.checkGetProductById,
+  ProductController.getProductById
+);
+
 export default productRoute;
