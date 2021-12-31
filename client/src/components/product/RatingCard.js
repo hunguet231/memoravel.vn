937a5bcd13b102fed19b42fdddd9b35d86de7200
hungUET231 <hungngv231@gmail.com>
2021-12-31 14:28:59 +0700

    fix deploy

--- client/src/components/product/RatingCard.js
+++ client/src/components/product/RatingCard.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Row, Col, Avatar, Rate } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Rate } from "antd";
+import React from "react";
 import styles from "styles/RatingCard.module.scss";
 
 export default function RatingCard() {
@@ -14,7 +14,12 @@ export default function RatingCard() {
           <div className={styles.wrapInfo}>
             <div className="flex justify-between">
               <span className={styles.customerName}>Trần Thị Bích Phượng</span>
-              <Rate disabled allowHalf defaultValue={4.5} style={{ color: "#F3692E" }} />
+              <Rate
+                disabled
+                allowHalf
+                defaultValue={4.5}
+                style={{ color: "#F3692E" }}
+              />
             </div>
             <p className={styles.dateRating}>2 tháng 10 năm 2021 at 06.30 pm</p>
             <div className={styles.wrapImg}>
@@ -32,8 +37,9 @@ export default function RatingCard() {
               </div>
             </div>
             <p className={styles.comment}>
-              Sản phẩm rất tuyệt, vợ tôi cực kì thích nó, từ khi mua nó thì chúng tôi đã kiếm được
-              rất nhiều tiền, tôi sẽ mua tặng cho bạn bè, cảm ơn shop.
+              Sản phẩm rất tuyệt, vợ tôi cực kì thích nó, từ khi mua nó thì
+              chúng tôi đã kiếm được rất nhiều tiền, tôi sẽ mua tặng cho bạn bè,
+              cảm ơn shop.
             </p>
           </div>
         </div>
