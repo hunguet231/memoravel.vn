9758feafe961a82e0b65274cb119e4d49f05655e
hungUET231 <hungngv231@gmail.com>
2022-01-06 18:12:08 +0700

    update blog UI

--- client/src/components/blog/BlogContent.js
+++ client/src/components/blog/BlogContent.js
@@ -44,25 +44,29 @@ export default function BlogContent({
             style={{ backgroundImage: `url(${background})` }}
           ></div>
           <div className="container">
-            <div className={styles.topic}>
-              {topics.map((topic) => (
-                <span key={topic.id} className="button-tag">
-                  {topic.title}
-                </span>
-              ))}
+            <div className={styles.blogContent}>
+              <div className={styles.topic}>
+                {topics.map((topic) => (
+                  <span key={topic.id} className="button-tag">
+                    {topic.title}
+                  </span>
+                ))}
+              </div>
+              <h1 className="heading heading-section">{title}</h1>
+              <p>
+                {" "}
+                {modified
+                  ? new Date(modified).toLocaleDateString()
+                  : new Date(created).toLocaleDateString()}{" "}
+                • {readingTime(content)} phút đọc • {number_view} lượt xem
+              </p>
+              <hr />
+              <i className={styles.description}>
+                {ReactHtmlParser(description)}
+              </i>
+              <div className={styles.content}>{ReactHtmlParser(content)}</div>
+              <div className={styles.credit}>MEMORAVEL.VN</div>
             </div>
-            <h1 className="heading heading-section">{title}</h1>
-            <p>
-              {" "}
-              {modified
-                ? new Date(modified).toLocaleDateString()
-                : new Date(created).toLocaleDateString()}{" "}
-              • {readingTime(content)} phút đọc • {number_view} lượt xem
-            </p>
-            <hr />
-            <i className={styles.description}>{ReactHtmlParser(description)}</i>
-            <div className={styles.content}>{ReactHtmlParser(content)}</div>
-            <div className={styles.credit}>MEMORAVEL.VN</div>
           </div>
         </div>
         <div className="container">
